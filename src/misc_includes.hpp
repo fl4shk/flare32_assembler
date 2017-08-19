e8ccfad2a58430a24204d630f38b7901d494e93d
fl4shk <fl4shk@users.noreply.github.com>
2017-08-19 07:55:20 -0400

    Some skeleton stuff

---
+++ src/misc_includes.hpp
@@ -0,0 +1,23 @@
+#ifndef misc_includes_hpp
+#define misc_includes_hpp
+
+
+#include "liborangepower_src/misc_includes.hpp"
+#include "liborangepower_src/misc_defines.hpp"
+#include "liborangepower_src/gen_getter_setter_defines.hpp"
+#include "liborangepower_src/misc_output_classes.hpp"
+#include "liborangepower_src/misc_bitwise_funcs.hpp"
+#include "liborangepower_src/misc_utility_funcs.hpp"
+#include "liborangepower_src/misc_types.hpp"
+#include "liborangepower_src/range_funcs.hpp"
+
+
+using namespace liborangepower::printout_etc;
+using namespace liborangepower::bitwise;
+using namespace liborangepower::misc_util;
+using namespace liborangepower::integer_types;
+using namespace liborangepower::range;
+
+
+
+#endif		// misc_includes_hpp
