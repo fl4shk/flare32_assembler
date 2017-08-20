6800d927d602a78ac113872418ad8e8a1fdf8450
fl4shk <fl4shk@users.noreply.github.com>
2017-08-20 10:59:54 -0400

    More stuffs

--- src/instruction_class.hpp
+++ src/instruction_class.hpp
@@ -14,148 +14,148 @@
 	// a:  rA (destination register usually)
 	// b:  rB
 
-#define LIST_OF_INSTR_G0_RA_RB__COLLECTION_0(VARNAME, VALUE) \
+#define LIST_OF_INSTR_G0_RA_RB__COLLECTION_0(ARGS, VARNAME, VALUE) \
 /* rA = rA + rB */ \
 /* This instruction can affect N, V, Z, and C flags. */ \
-VARNAME(Add) VALUE("add") \
+ARGS(ra_rb) VARNAME(Add) VALUE("add") \
 \
 /* rA = rA + rB + carry_flag */ \
 /* Add with carry */ \
 /* This instruction can affect N, V, Z, and C flags. */ \
-VARNAME(Adc) VALUE("adc") \
+ARGS(ra_rb) VARNAME(Adc) VALUE("adc") \
 \
 /* rA = rA - rB */ \
 /* This instruction can affect N, V, Z, and C flags. */ \
-VARNAME(Sub) VALUE("sub") \
+ARGS(ra_rb) VARNAME(Sub) VALUE("sub") \
 \
 /* rA = rA + (~rB) + carry_flag */ \
 /* Subtract with borrow (6502 style) */ \
 /* This instruction can affect N, V, Z, and C flags. */ \
-VARNAME(Sbc) VALUE("sbc") \
+ARGS(ra_rb) VARNAME(Sbc) VALUE("sbc") \
 \
 \
 \
 /* rA = rB - rA */ \
 /* This instruction can affect N, V, Z, and C flags. */ \
-VARNAME(Rsb) VALUE("rsb") \
+ARGS(ra_rb) VARNAME(Rsb) VALUE("rsb") \
 \
 /* rA = rA * rB */ \
-VARNAME(Mul) VALUE("mul") \
+ARGS(ra_rb) VARNAME(Mul) VALUE("mul") \
 \
 /* rA = rA & rB */ \
 /* This instruction can affect the N and Z flags. */ \
-VARNAME(And) VALUE("and") \
+ARGS(ra_rb) VARNAME(And) VALUE("and") \
 \
 /* rA = rA | rB */ \
 /* This instruction can affect the N and Z flags. */ \
-VARNAME(Or) VALUE("or") \
+ARGS(ra_rb) VARNAME(Or) VALUE("or") \
 \
 \
 \
 /* rA = rA ^ rB */ \
 /* This instruction can affect the N and Z flags. */ \
-VARNAME(Xor) VALUE("xor") \
+ARGS(ra_rb) VARNAME(Xor) VALUE("xor") \
 \
 /* Logical shift left */ \
 /* rA = rA << rB */ \
-VARNAME(Lsl) VALUE("lsl") \
+ARGS(ra_rb) VARNAME(Lsl) VALUE("lsl") \
 \
 /* Logical shift right */ \
-VARNAME(Lsr) VALUE("lsr") \
+ARGS(ra_rb) VARNAME(Lsr) VALUE("lsr") \
 \
 /* Arithmetic shift right */ \
-VARNAME(Asr) VALUE("asr") \
+ARGS(ra_rb) VARNAME(Asr) VALUE("asr") \
 \
 \
 \
 /* Rotate rA left by rB bits, then store result in rA. */ \
-VARNAME(Rol) VALUE("rol") \
+ARGS(ra_rb) VARNAME(Rol) VALUE("rol") \
 \
 /* Rotate rA right by rB bits, then store result in rA. */ \
-VARNAME(Ror) VALUE("ror") \
+ARGS(ra_rb) VARNAME(Ror) VALUE("ror") \
 \
 /* Rotate rB left (THROUGH carry) by 1 bit, then store result in rA. */ \
 /* This instruction can only affect the C flag. */ \
-VARNAME(Rlc) VALUE("rlc") \
+ARGS(ra_rb) VARNAME(Rlc) VALUE("rlc") \
 \
 /* Rotate rB right (THROUGH carry) by 1 bit, then store result in rA. */ \
 /* This instruction can only affect the C flag. */ \
-VARNAME(Rrc) VALUE("rrc") \
+ARGS(ra_rb) VARNAME(Rrc) VALUE("rrc") \
 \
 \
 \
 /* rA = Sign extend of low 16 bits in rB */ \
 /* Note that the high 16 bits of rB are ignored */ \
-VARNAME(Seh) VALUE("seh") \
+ARGS(ra_rb) VARNAME(Seh) VALUE("seh") \
 \
 /* rA = Sign extend of low 8 bits in rB */ \
-VARNAME(Seb) VALUE("seb") \
+ARGS(ra_rb) VARNAME(Seb) VALUE("seb") \
 \
 /* Push registers rA and rb (as a pair) onto the  */ \
 /* stack (in that order) */ \
-VARNAME(Push) VALUE("push") \
+ARGS(ra_rb) VARNAME(Push) VALUE("push") \
 \
 /* Pop two 32-bit values off the stack, storing the first popped */ \
 /* value into rB and the second popped value into rA */ \
-VARNAME(Pop) VALUE("pop")
+ARGS(ra_rb) VARNAME(Pop) VALUE("pop")
 
 
 
-#define LIST_OF_INSTR_G0_FLAGS__COLLECTION_0(VARNAME, VALUE) \
+#define LIST_OF_INSTR_G0_FLAGS__COLLECTION_0(ARGS, VARNAME, VALUE) \
 /* Push flags as 8-bit value onto the stack (high 4 bits of pushed */ \
 /* value are set to zero before the push) */ \
-VARNAME(Push) VALUE("push") \
+ARGS(flags) VARNAME(Push) VALUE("push") \
 \
 /* Pop 8-bit value from the stack and store low 4 bits to flags */ \
-VARNAME(Pop) VALUE("pop")
+ARGS(flags) VARNAME(Pop) VALUE("pop")
 
 
-#define LIST_OF_INSTR_G0_RA_FLAGS__COLLECTION_0(VARNAME, VALUE) \
+#define LIST_OF_INSTR_G0_RA_FLAGS__COLLECTION_0(ARGS, VARNAME, VALUE) \
 /* Clear rA, then CoPY FRom Flags to rA */ \
-VARNAME(Cpy) VALUE("cpy")
+ARGS(ra_flags) VARNAME(Cpy) VALUE("cpy")
 
-#define LIST_OF_INSTR_G0_FLAGS_RA__COLLECTION_0(VARNAME, VALUE) \
+#define LIST_OF_INSTR_G0_FLAGS_RA__COLLECTION_0(ARGS, VARNAME, VALUE) \
 /* Copy to flags from rA */ \
 /* This instruction can affect N, V, Z, and C flags. */ \
-VARNAME(Cpy) VALUE("cpy")
+ARGS(flags_ra) VARNAME(Cpy) VALUE("cpy")
 
 
-#define LIST_OF_INSTR_G0_NO_ARGS__COLLECTION_0(VARNAME, VALUE) \
+#define LIST_OF_INSTR_G0_NO_ARGS__COLLECTION_0(ARGS, VARNAME, VALUE) \
 /* ENable Interrupts */ \
-VARNAME(Eni) VALUE("eni") \
+ARGS(no_args) VARNAME(Eni) VALUE("eni") \
 \
 /* DIsable Interrupts */ \
-VARNAME(Dii) VALUE("dii") \
+ARGS(no_args) VARNAME(Dii) VALUE("dii") \
 \
 /* Set the PC to interrupt RETurn address and enable Interrupts */ \
-VARNAME(Reti) VALUE("reti") \
+ARGS(no_args) VARNAME(Reti) VALUE("reti") \
 
 
-#define LIST_OF_INSTR_G0_IRA__COLLECTION_0(VARNAME, VALUE) \
+#define LIST_OF_INSTR_G0_IRA__COLLECTION_0(ARGS, VARNAME, VALUE) \
 /* Set the PC to the Interrupt Return Address, but DON'T enable */ \
 /* interrupts */ \
-VARNAME(Jump) VALUE("jump")
+ARGS(ira) VARNAME(Jump) VALUE("jump")
 
 
 
-#define LIST_OF_INSTR_G0_IRA_RA__COLLECTION_0(VARNAME, VALUE) \
+#define LIST_OF_INSTR_G0_IRA_RA__COLLECTION_0(ARGS, VARNAME, VALUE) \
 /* Copy rA to the Interrupt Return Address */ \
-VARNAME(Cpy) VALUE("cpy")
+ARGS(ira_ra) VARNAME(Cpy) VALUE("cpy")
 
-#define LIST_OF_INSTR_G0_RA_IRA__COLLECTION_0(VARNAME, VALUE) \
+#define LIST_OF_INSTR_G0_RA_IRA__COLLECTION_0(ARGS, VARNAME, VALUE) \
 /* Copy the Interrupt Return Address to rA */ \
-VARNAME(Cpy) VALUE("cpy")
+ARGS(ra_ira) VARNAME(Cpy) VALUE("cpy")
 
 
-#define LIST_OF_INSTR_G0_RA_RB__COLLECTION_1(VARNAME, VALUE) \
+#define LIST_OF_INSTR_G0_RA_RB__COLLECTION_1(ARGS, VARNAME, VALUE) \
 /* Subroutine call using (rA + rB) as destination address.  */ \
 /* The return address is stored in the link register (lr). */ \
-VARNAME(Callx) VALUE("callx")
+ARGS(ra_rb) VARNAME(Callx) VALUE("callx")
 
-#define LIST_OF_INSTR_G0_RA_PC__COLLECTION_0(VARNAME, VALUE) \
+#define LIST_OF_INSTR_G0_RA_PC__COLLECTION_0(ARGS, VARNAME, VALUE) \
 /* Copy pc to rA (this can be used for pc-relative loads */ \
 /* and stores) */ \
-VARNAME(Cpy) VALUE("cpy")
+ARGS(ra_pc) VARNAME(Cpy) VALUE("cpy")
 
 
 
