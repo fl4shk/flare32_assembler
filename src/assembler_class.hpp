896d261976e9db66b65b8248b3696c3c861b959f
fl4shk <fl4shk@users.noreply.github.com>
2017-09-29 19:16:40 -0400

    More stuffs

--- src/assembler_class.hpp
+++ src/assembler_class.hpp
@@ -14,6 +14,31 @@ namespace flare32
 class Assembler
 {
 private:		// classes
+	class ParseNode
+	{
+	public:		// variables
+		PTok next_tok = nullptr;
+		std::string next_sym_str;
+		s64 next_num = -1;
+
+	public:		// functions
+		inline ParseNode()
+		{
+		}
+
+
+		inline ParseNode(PTok s_next_tok, 
+			const std::string& s_next_sym_str, s64 s_next_num)
+			: next_tok(s_next_tok), next_sym_str(s_next_sym_str),
+			next_num(s_next_num)
+		{
+		}
+
+		inline ParseNode(const ParseNode& to_copy) = default;
+		inline ParseNode(ParseNode&& to_move) = default;
+		inline ParseNode& operator = (const ParseNode& to_copy) = default;
+		inline ParseNode& operator = (ParseNode&& to_move) = default;
+	};
 
 private:		// variables
 	SymbolTable __builtin_sym_tbl, __user_sym_tbl;
